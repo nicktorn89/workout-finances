34edbedb1ad42f0d77a27d23ed3229d027361ee4
vlad-komanchy <vkomanchy@gmail.com>
2019-04-25 22:26:36 +0300

    feat: add slider for month

---
+++ src/components/Slider/styled.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { Typography } from '@material-ui/core';
+
+// @ts-nocheck
+
+export const SliderContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 65px;
+`;
+
+export const DateTitle: any = styled(Typography)`
+  margin: 0 2rem !important;
+  font-size: 14px !important;
+  font-weight: 700 !important;
+`;
