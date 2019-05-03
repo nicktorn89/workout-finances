79f6f9062cc5e82d2294ff62c2c84838e67c2128
vlad-komanchy <vkomanchy@gmail.com>
2019-05-03 18:56:16 +0300

    feat: adding snapshots from Enzyme

--- workout-finances-gui/src/modules/Main/__tests__/Main.test.tsx
+++ workout-finances-gui/src/modules/Main/__tests__/Main.test.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Main from '../Main';
+import { shallowToJson } from 'enzyme-to-json';
 
 describe('<Main />', () => {
-  it('renders Main component', () => {
-    const MainComponent = shallow(<Main />);
+  const MainComponent = shallow(<Main />);
+  const MainComponentToJson = shallowToJson(MainComponent);
 
+  it('renders Main component', () => {
     expect(MainComponent).toExist();
+  });
+
+  it('should contain only one h2 title with text - Workout Finances', () => {
+    expect(MainComponent).toContainExactlyOneMatchingElement('.header-title');
+  });
+
+  it('should be equal to snapshot', () => {
+    expect(MainComponentToJson).toMatchSnapshot();
   });
 });
